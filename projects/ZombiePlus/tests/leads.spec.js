fe325631792304ac725ce27aaf33fc9304d52612
BiancaFSilva <biancaflorianodasilva@gmail.com>
2025-07-01 22:42:19 -0300

    Get button via getByRole action to lead sign-in test

--- projects/ZombiePlus/tests/leads.spec.js
+++ projects/ZombiePlus/tests/leads.spec.js
@@ -4,7 +4,7 @@ import { test, expect } from '@playwright/test';
 test('should sign in a lead in the waiting list', async ({ page }) => {
   await page.goto('http://localhost:3000');
 
-  await page.click('//button[text()="Aperte o play... se tiver coragem"]')
+  await page.getByRole('button', {name: 'Aperte o play... se tiver coragem'}).click();
 
   await page.waitForTimeout(10000);
 });
