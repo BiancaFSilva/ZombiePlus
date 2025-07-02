baccf344e420dae4c41a2881df0a42df88a44dc1
BiancaFSilva <biancaflorianodasilva@gmail.com>
2025-07-01 22:40:40 -0300

    Get button via x-path to lead sign-in test

--- projects/ZombiePlus/tests/leads.spec.js
+++ projects/ZombiePlus/tests/leads.spec.js
@@ -3,4 +3,8 @@ import { test, expect } from '@playwright/test';
 
 test('should sign in a lead in the waiting list', async ({ page }) => {
   await page.goto('http://localhost:3000');
+
+  await page.click('//button[text()="Aperte o play... se tiver coragem"]')
+
+  await page.waitForTimeout(10000);
 });
