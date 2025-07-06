9973f607cbb164c7c6468c5371202f4e32345633
BiancaFSilva <biancaflorianodasilva@gmail.com>
2025-07-06 17:26:12 -0300

    Get elements by data-test-id using expect

--- projects/tests/leads.spec.js
+++ projects/tests/leads.spec.js
@@ -5,6 +5,9 @@ test('should sign in a lead in the waiting list', async ({ page }) => {
   // Arrange
   await page.goto('http://localhost:3000');
   await page.getByRole('button', {name: /Aperte o play/}).click();
+  await expect(
+    page.getByTestId('modal').getByRole('heading')
+  ).toHaveText('Fila de espera');
 
   // Act
   await page.locator('input[name=name]').fill('Nome Completo')
