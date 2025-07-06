a2c5fe62895d36e6061ee5e7d994f324c4cbd86e
BiancaFSilva <biancaflorianodasilva@gmail.com>
2025-07-06 18:38:47 -0300

    Add tests for lead sign-in validation scenarios

--- projects/tests/leads.spec.js
+++ projects/tests/leads.spec.js
@@ -20,3 +20,71 @@ test('should sign in a lead in the waiting list', async ({ page }) => {
   await expect(await page.locator('.toast')).toHaveText(message);
   await expect(await page.locator('.toast')).toBeHidden({timeout: 5000});
 });
+
+test('should not sign in a lead with invalid email', async ({ page }) => {
+  // Arrange
+  await page.goto('http://localhost:3000');
+  await page.getByRole('button', {name: /Aperte o play/}).click();
+  await expect(
+    page.getByTestId('modal').getByRole('heading')
+  ).toHaveText('Fila de espera');
+
+  // Act
+  await page.locator('input[name=name]').fill('Nome Completo')
+  await page.locator('input[name=email]').fill('email.exemplo.com')
+  await page.getByTestId('modal')
+    .getByRole('button', {name: 'Quero entrar na fila!'}).click();
+
+  // Assert
+  await expect(page.locator('.alert')).toHaveText("Email incorreto");
+});
+
+test('should not sign in a lead with empty name', async ({ page }) => {
+  // Arrange
+  await page.goto('http://localhost:3000');
+  await page.getByRole('button', {name: /Aperte o play/}).click();
+  await expect(
+    page.getByTestId('modal').getByRole('heading')
+  ).toHaveText('Fila de espera');
+
+  // Act
+  await page.locator('input[name=email]').fill('email@exemplo.com')
+  await page.getByTestId('modal')
+    .getByRole('button', {name: 'Quero entrar na fila!'}).click();
+
+  // Assert
+  await expect(page.locator('.alert')).toHaveText("Campo obrigatório");
+});
+
+test('should not sign in a lead with empty email', async ({ page }) => {
+  // Arrange
+  await page.goto('http://localhost:3000');
+  await page.getByRole('button', {name: /Aperte o play/}).click();
+  await expect(
+    page.getByTestId('modal').getByRole('heading')
+  ).toHaveText('Fila de espera');
+
+  // Act
+  await page.locator('input[name=name]').fill('Nome Completo')
+  await page.getByTestId('modal')
+    .getByRole('button', {name: 'Quero entrar na fila!'}).click();
+
+  // Assert
+  await expect(page.locator('.alert')).toHaveText("Campo obrigatório");
+});
+
+test('should not sign in a lead without filling name and email', async ({ page }) => {
+  // Arrange
+  await page.goto('http://localhost:3000');
+  await page.getByRole('button', {name: /Aperte o play/}).click();
+  await expect(
+    page.getByTestId('modal').getByRole('heading')
+  ).toHaveText('Fila de espera');
+
+  // Act
+  await page.getByTestId('modal')
+    .getByRole('button', {name: 'Quero entrar na fila!'}).click();
+
+  // Assert
+  await expect(page.locator('.alert')).toHaveText(['Campo obrigatório', 'Campo obrigatório']);
+});
